99b5bde34b39c9e67cc8eb6428a38b60bba6b916
Nakio82 <ignaciogalasso82@gmail.com>
2022-01-05 18:19:09 -0300

    components

---
+++ 09_primer_cli/src/components/Card.vue
@@ -0,0 +1,36 @@
+<template>
+  <section id="card" class="card">
+      <h4>Name Project</h4>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis labore dolores et dolorem, eligendi quasi odit, fuga voluptates consectetur quam, illum voluptatibus rerum sunt totam. Ullam a laboriosam ducimus commodi!</p>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 30rem;
+    border: .2rem solid rgb(151, 150, 150);
+    border-radius: 1rem;
+    padding: .5rem 1rem;
+}
+
+.card h4 {
+    font-size: 1.8rem;
+    font-variant: small-caps;
+    font-weight: 700;
+}
+
+.card p {
+    font-size: 1.2rem
+}
+
+
+</style>
---
+++ 09_primer_cli/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <h1> {{title}} </h1>
+    </header>
+</template>
+
+
+
+<script>
+
+export default {
+  props: {
+      title: String,
+  }
+}
+
+</script>
+
+
+<style scoped>
+  h1 {
+    background-color: rgba(255, 188, 150, 0.6);
+    color: rgb(24, 1, 1);
+    padding: .5rem;
+    font-size: 4rem;
+    text-align: center;
+    font-family: 'Lato', Arial, Helvetica, sans-serif;
+  }
+</style>
